9f6ad9da796e7ff35a744ca4751c0ea778184f3a
Chankanika Soam <csoam@asu.edu>
2024-05-06 20:48:06 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! I was changed!
+          Hello World! I was changed!!
         </p>
         <a
           className="App-link"
